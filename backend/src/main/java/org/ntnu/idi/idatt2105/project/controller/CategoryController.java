d893e489fcbd717a1ae978d38123647fd3ecdfab
Ina <inamar@stud.ntnu.no>
2024-04-02 09:41:49 +0200

    added a CrossOrigin reference.

--- backend/src/main/java/org/ntnu/idi/idatt2105/project/controller/CategoryController.java
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/controller/CategoryController.java
@@ -8,13 +8,10 @@ import org.ntnu.idi.idatt2105.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("api/categories")
 public class CategoryController {
 
@@ -38,7 +35,7 @@ public class CategoryController {
                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @GetMapping
+    @GetMapping("/allCategories")
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.findAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
