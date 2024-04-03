857532742d0e6597ba97f7e3c30fc902fcc26d5b
Eline Evje <elineev@stud.ntnu.no>
2024-04-03 09:44:03 +0200

    Sonar lint spotless fixes

--- backend/src/main/java/org/ntnu/idi/idatt2105/project/service/QuestionTypeService.java
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/service/QuestionTypeService.java
@@ -2,7 +2,6 @@ package org.ntnu.idi.idatt2105.project.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.ntnu.idi.idatt2105.project.entity.QuestionType;
 import org.ntnu.idi.idatt2105.project.repository.QuestionTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class QuestionTypeService {
 
+    private final QuestionTypeRepository questionTypeRepository;
+
     @Autowired
-    private QuestionTypeRepository questionTypeRepository;
+    public QuestionTypeService(QuestionTypeRepository questionTypeRepository) {
+        this.questionTypeRepository = questionTypeRepository;
+    }
 
     public QuestionType createQuestionType(String type) {
         QuestionType questionType = new QuestionType();
