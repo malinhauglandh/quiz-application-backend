e0bc4f2c7efb5952e52450a1c993cdbfcd112c70
Ina <inamar@stud.ntnu.no>
2024-04-04 17:40:47 +0200

    Update all controllers with swagger documentation.

--- backend/src/main/java/org/ntnu/idi/idatt2105/project/controller/CategoryController.java
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/controller/CategoryController.java
@@ -3,6 +3,12 @@ package org.ntnu.idi.idatt2105.project.controller;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.ntnu.idi.idatt2105.project.dto.CategoryDTO;
 import org.ntnu.idi.idatt2105.project.entity.Category;
 import org.ntnu.idi.idatt2105.project.repository.CategoryRepository;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("api/categories")
+@Tag(name = "Category Management", description = "Endpoints for managing categories")
 public class CategoryController {
 
     private final CategoryService categoryService;
@@ -27,12 +34,30 @@ public class CategoryController {
         this.categoryRepository = categoryRepository;
     }
 
+    @Operation(summary = "Create a new category",
+        parameters = {
+            @Parameter(name = "categoryName",
+                description = "The name of the category")
+        },
+        responses = {
+            @ApiResponse(responseCode = "201", description = "Category created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+        })
     @PostMapping
     public ResponseEntity<Category> createCategory(@RequestBody String categoryName) {
         Category category = categoryService.createCategory(categoryName);
         return new ResponseEntity<>(category, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get a category by id",
+        parameters = {
+            @Parameter(name = "id",
+                description = "Category id")
+        },
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Category found"),
+            @ApiResponse(responseCode = "404", description = "Category not found")
+        })
     @GetMapping("/{id}")
     public ResponseEntity<Category> getCategory(@PathVariable Long id) {
         Optional<Category> category = categoryService.findCategoryById(id);
@@ -40,6 +65,11 @@ public class CategoryController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Get all categories",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Categories found"),
+            @ApiResponse(responseCode = "404", description = "Categories not found")
+        })
     @GetMapping("/allCategories")
     public List<CategoryDTO> findAllCategories() {
         return categoryRepository.findAll().stream()
