36475b62f8f72c63d9a5c9cbfb3ca646171767ca
Ina <inamar@stud.ntnu.no>
2024-04-05 12:08:41 +0200

    Update UserAnswerRepository with method for finding user answers with completed quiz id.

--- backend/src/main/java/org/ntnu/idi/idatt2105/project/repository/UserAnswerRepository.java
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/repository/UserAnswerRepository.java
@@ -3,5 +3,9 @@ package org.ntnu.idi.idatt2105.project.repository;
 import org.ntnu.idi.idatt2105.project.entity.UserAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /** Repository interface for the UserAnswer entity. */
-public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {}
+public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
+  List<UserAnswer> findByCompletedQuizCompletedQuizId(Long completedQuizId);
+}
