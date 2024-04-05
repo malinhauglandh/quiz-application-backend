24d0011d2c8be5e300a2391e55cd84e24d429c10
Ina <inamar@stud.ntnu.no>
2024-04-05 12:06:03 +0200

    Add a UserAnswerDTO for transferring objects.

---
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/dto/UserAnswerDTO.java
@@ -0,0 +1,17 @@
+package org.ntnu.idi.idatt2105.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data transfer object for UserAnswer entity.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAnswerDTO {
+  private Long userAnswerId;
+  private Long completedQuizId;
+  private Long questionChoiceId;
+}
