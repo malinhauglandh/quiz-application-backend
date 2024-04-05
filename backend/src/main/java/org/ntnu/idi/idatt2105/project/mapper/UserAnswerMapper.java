c6966b2463c2e822f7a875526c098941f38febd8
Ina <inamar@stud.ntnu.no>
2024-04-05 12:06:47 +0200

    Add a CompletedQuizMapper for convert user answers to DTO before sending data to the frontend.

---
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/mapper/UserAnswerMapper.java
@@ -0,0 +1,25 @@
+package org.ntnu.idi.idatt2105.project.mapper;
+
+import org.ntnu.idi.idatt2105.project.dto.UserAnswerDTO;
+import org.ntnu.idi.idatt2105.project.entity.UserAnswer;
+import org.springframework.stereotype.Component;
+
+/**
+ * Mapper class for the UserAnswer entity. Converts UserAnswer entities to UserAnswerDTOs.
+ * It is used to convert UserAnswer entities to UserAnswerDTOs before sending the data to the frontend.
+ */
+@Component
+public class UserAnswerMapper {
+  public UserAnswerDTO convertUserAnswerToDTO(UserAnswer userAnswer) {
+    if (userAnswer == null) {
+      return null;
+    }
+
+    UserAnswerDTO dto = new UserAnswerDTO();
+    dto.setUserAnswerId((long) userAnswer.getAnswerId());
+    dto.setCompletedQuizId(userAnswer.getCompletedQuiz() != null ?(long) userAnswer.getCompletedQuiz().getCompletedQuizId() : null);
+    dto.setQuestionChoiceId(userAnswer.getQuestionChoice() != null ? (long)userAnswer.getQuestionChoice().getQuizChoiceId() : null);
+
+    return dto;
+  }
+}
