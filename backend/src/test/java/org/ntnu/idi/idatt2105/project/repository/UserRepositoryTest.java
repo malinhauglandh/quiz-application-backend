5508a0f4d9bf3a3b58c2b2c16e738f92d1b3554c
Eline Evje <elineev@stud.ntnu.no>
2024-04-05 22:47:21 +0200

    made unit tests for user repository, controller and service

--- backend/src/test/java/org/ntnu/idi/idatt2105/project/repository/UserRepositoryTest.java
+++ backend/src/test/java/org/ntnu/idi/idatt2105/project/repository/UserRepositoryTest.java
@@ -1,17 +1,44 @@
 package org.ntnu.idi.idatt2105.project.repository;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.ntnu.idi.idatt2105.project.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.ntnu.idi.idatt2105.project.entity.user.User;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class UserRepositoryTest {
 
-    @Autowired private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
-    public void testMyMethod() {}
+    public void verifyFindByUsernameReturnUser() {
+        User user = new User();
+        user.setUsername("testUser");
+        user.setPassword("password");
+        user.setEmail("test@user.com");
+        userRepository.save(user);
+
+        Optional<User> foundUser = userRepository.findByUsername(user.getUsername());
+
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getUsername()).isEqualTo(user.getUsername());
+    }
+
+    @Test
+    public void verifyFindByUsernameWithNonExistingUserReturnEmpty() {
+        Optional<User> foundUser = userRepository.findByUsername("nonExistingUser");
+
+        assertThat(foundUser).isNotPresent();
+    }
 }
