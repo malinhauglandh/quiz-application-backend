4ef5e82e3dd045db7ef6c48b1479e1e5db140436
Ina <inamar@stud.ntnu.no>
2024-04-05 12:08:13 +0200

    Update CompletedQuizRepository with method for finding completed quizzes from user and quiz id.

--- backend/src/main/java/org/ntnu/idi/idatt2105/project/repository/CompletedQuizRepository.java
+++ backend/src/main/java/org/ntnu/idi/idatt2105/project/repository/CompletedQuizRepository.java
@@ -1,7 +1,13 @@
 package org.ntnu.idi.idatt2105.project.repository;
 
 import org.ntnu.idi.idatt2105.project.entity.CompletedQuiz;
+import org.ntnu.idi.idatt2105.project.entity.Quiz;
+import org.ntnu.idi.idatt2105.project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /** Repository interface for the CompletedQuiz entity. */
-public interface CompletedQuizRepository extends JpaRepository<CompletedQuiz, Long> {}
+public interface CompletedQuizRepository extends JpaRepository<CompletedQuiz, Long> {
+  List<CompletedQuiz> findByUserAndQuiz(User quizId, Quiz userId);
+}
