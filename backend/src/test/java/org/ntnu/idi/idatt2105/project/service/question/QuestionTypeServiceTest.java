c4d8bb357d1f47773f0948bbb48ccf6afca284ee
Eline Evje <elineev@stud.ntnu.no>
2024-04-08 04:06:46 +0200

    spotless fixes

--- backend/src/test/java/org/ntnu/idi/idatt2105/project/service/question/QuestionTypeServiceTest.java
+++ backend/src/test/java/org/ntnu/idi/idatt2105/project/service/question/QuestionTypeServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,58 +21,60 @@ import org.springframework.test.context.ActiveProfiles;
 @ExtendWith(MockitoExtension.class)
 class QuestionTypeServiceTest {
 
-    @Mock private QuestionTypeRepository questionTypeRepository;
+  @Mock
+  private QuestionTypeRepository questionTypeRepository;
 
-    @InjectMocks private QuestionTypeService questionTypeService;
+  @InjectMocks
+  private QuestionTypeService questionTypeService;
 
-    @Test
-    void verifyCreateQuestionType() {
-        String type = "Test question type";
-        QuestionType questionType = new QuestionType();
-        questionType.setType(type);
+  @Test
+  void verifyCreateQuestionType() {
+    String type = "Test question type";
+    QuestionType questionType = new QuestionType();
+    questionType.setType(type);
 
-        when(questionTypeRepository.save(any(QuestionType.class))).thenReturn(questionType);
+    when(questionTypeRepository.save(any(QuestionType.class))).thenReturn(questionType);
 
-        QuestionType createdType = questionTypeService.createQuestionType(type);
+    QuestionType createdType = questionTypeService.createQuestionType(type);
 
-        assertEquals(type, createdType.getType());
-        verify(questionTypeRepository).save(any(QuestionType.class));
-    }
+    assertEquals(type, createdType.getType());
+    verify(questionTypeRepository).save(any(QuestionType.class));
+  }
 
-    @Test
-    void verifyFindAllTypesReturnAllTypes() {
-        List<QuestionType> types = Arrays.asList(new QuestionType(), new QuestionType());
+  @Test
+  void verifyFindAllTypesReturnAllTypes() {
+    List<QuestionType> types = Arrays.asList(new QuestionType(), new QuestionType());
 
-        when(questionTypeRepository.findAll()).thenReturn(types);
+    when(questionTypeRepository.findAll()).thenReturn(types);
 
-        List<QuestionType> foundTypes = questionTypeService.findAllTypes();
+    List<QuestionType> foundTypes = questionTypeService.findAllTypes();
 
-        assertEquals(2, foundTypes.size());
-        verify(questionTypeRepository).findAll();
-    }
+    assertEquals(2, foundTypes.size());
+    verify(questionTypeRepository).findAll();
+  }
 
-    @Test
-    void verifyFindTypeByIdReturnsTypeWithCorrectId() {
-        Long typeId = 1L;
-        QuestionType questionType = new QuestionType();
-        questionType.setTypeId(typeId);
+  @Test
+  void verifyFindTypeByIdReturnsTypeWithCorrectId() {
+    Long typeId = 1L;
+    QuestionType questionType = new QuestionType();
+    questionType.setTypeId(typeId);
 
-        when(questionTypeRepository.findById(typeId)).thenReturn(Optional.of(questionType));
+    when(questionTypeRepository.findById(typeId)).thenReturn(Optional.of(questionType));
 
-        QuestionType foundType = questionTypeService.findTypeByID(typeId);
+    QuestionType foundType = questionTypeService.findTypeByID(typeId);
 
-        assertEquals(typeId, foundType.getTypeId());
-        verify(questionTypeRepository).findById(typeId);
-    }
+    assertEquals(typeId, foundType.getTypeId());
+    verify(questionTypeRepository).findById(typeId);
+  }
 
-    @Test
-    void verifyFindTypeByIDWhenIdNotFound() {
-        Long typeId = 2L;
-        when(questionTypeRepository.findById(typeId)).thenReturn(Optional.empty());
+  @Test
+  void verifyFindTypeByIDWhenIdNotFound() {
+    Long typeId = 2L;
+    when(questionTypeRepository.findById(typeId)).thenReturn(Optional.empty());
 
-        QuestionType foundType = questionTypeService.findTypeByID(typeId);
+    QuestionType foundType = questionTypeService.findTypeByID(typeId);
 
-        assertNull(foundType);
-        verify(questionTypeRepository).findById(typeId);
-    }
+    assertNull(foundType);
+    verify(questionTypeRepository).findById(typeId);
+  }
 }
